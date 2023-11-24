d4c73191af1741fefe14c7773c5762d877862898
Ven <56719881+ven2day@users.noreply.github.com>
2023-11-24 20:24:26 +0200

    --added imports

--- projects/dagster/my_dbt.py
+++ projects/dagster/my_dbt.py
@@ -3,53 +3,53 @@ from dagster import job, op
 from dagster_dbt import DbtCliResource
 
 # Import the dbt module dynamically
-dbt_module = importlib.import_module('dbt')
+dbt_module = importlib.import_module('my_dbt')
 
 # Define dbt operations that use the dbt resource
-@op(required_resource_keys={'dbt'})
+@op(required_resource_keys={'my_dbt'})
 def dbt_bronze_run_op(context, dependent_job=None):
     dbt_module._dbt_run(context)
 
-@op(required_resource_keys={'dbt'})
+@op(required_resource_keys={'my_dbt'})
 def dbt_bronze_test_doc_sources_op(context, dependent_job=None):
     dbt_module._dbt_test(context)
     dbt_module._dbt_generate_docs(context)
     dbt_module._dbt_source_freshness(context)
 
-@op(required_resource_keys={'dbt'})
+@op(required_resource_keys={'my_dbt'})
 def dbt_silver_run_op(context, dependent_job=None):
     dbt_module._dbt_run(context)
 
-@op(required_resource_keys={'dbt'})
+@op(required_resource_keys={'my_dbt'})
 def dbt_silver_test_doc_sources_op(context, dependent_job=None):
     dbt_module._dbt_test(context)
     dbt_module._dbt_generate_docs(context)
     dbt_module._dbt_source_freshness(context)
 
-@op(required_resource_keys={'dbt'})
+@op(required_resource_keys={'my_dbt'})
 def dbt_gold_run_op(context, dependent_job=None):
     dbt_module._dbt_run(context)
 
-@op(required_resource_keys={'dbt'})
+@op(required_resource_keys={'my_dbt'})
 def dbt_gold_test_doc_sources_op(context, dependent_job=None):
     dbt_module._dbt_test(context)
     dbt_module._dbt_generate_docs(context)
     dbt_module._dbt_source_freshness(context)
 
 # Define jobs that use the dbt resource
-@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
+@job(resource_defs={'my_dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_bronze():
     dbt_bronze_test_doc_sources_op(dbt_bronze_run_op())
 
-@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
+@job(resource_defs={'my_dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_silver():    
     dbt_silver_test_doc_sources_op(dbt_silver_run_op())
 
-@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
+@job(resource_defs={'my_dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_gold():    
     dbt_gold_test_doc_sources_op(dbt_gold_run_op())
 
-@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
+@job(resource_defs={'my_dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_all():    
     dbt_gold_test_doc_sources_op(
         dbt_gold_run_op(
--- projects/dagster/workspace.py
+++ projects/dagster/workspace.py
@@ -3,13 +3,13 @@ from dagster_pyspark import pyspark_resource
 
 from db import initialize_db, drop_tables_op, create_schemas_op, trino_resource
 from download import download_yahoo_finance_files, download_yahoo_finance_files_op
-from dbt import dbt_bronze, dbt_silver, dbt_gold, dbt_all, dbt_bronze_run_op, dbt_silver_run_op, dbt_gold_run_op, dbt_bronze_test_doc_sources_op, dbt_silver_test_doc_sources_op, dbt_gold_test_doc_sources_op
+from projects.dagster.my_dbt import dbt_bronze, dbt_silver, dbt_gold, dbt_all, dbt_bronze_run_op, dbt_silver_run_op, dbt_gold_run_op, dbt_bronze_test_doc_sources_op, dbt_silver_test_doc_sources_op, dbt_gold_test_doc_sources_op
 from predict import predict_op
 
 @job
 def e2e():    
     from dagster_dbt import DbtCliResource  # Move the import here
-    resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir'), 'trino': trino_resource, 'pyspark': pyspark_resource}
+    resource_defs={'my_dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir'), 'trino': trino_resource, 'pyspark': pyspark_resource}
     dbt_gold_test_doc_sources_op(
         dbt_gold_run_op(
             predict_op(
