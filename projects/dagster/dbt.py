823792ed6e2445850684d86195e0404d64ea4225
Ven <56719881+ven2day@users.noreply.github.com>
2023-11-24 15:15:08 +0200

    added configs

--- projects/dagster/dbt.py
+++ projects/dagster/dbt.py
@@ -1,59 +1,60 @@
 from dagster import job, op
+from dagster_dbt import DbtCliResource
 
-from dagster_dbt import dbt_cli_resource
-
-
+# Define dbt operations
 def _dbt_run(context):
-    pdir = context.op_config['project_dir']
+    # Get the project directory from the dbt resource
+    pdir = context.resources.dbt.project_dir
     context.log.info(f"elt: executing dbt run with project_dir: '{pdir}'")
     if 'select' in context.op_config:
         s = context.op_config['select']        
         context.log.info(f"elt: executing dbt run with select: '{s}'")
-        context.resources.dbt.cli("run", project_dir=pdir, select=s)
+        # Run dbt with the given selection
+        context.resources.dbt.cli("run", select=s)
     else:
-        context.resources.dbt.cli("run", project_dir=pdir)
+        # Run dbt without a specific selection
+        context.resources.dbt.cli("run")
 
 def _dbt_test(context):
-    pdir = context.op_config['project_dir']
+    pdir = context.resources.dbt.project_dir
     context.log.info(f"elt: executing dbt test with project_dir: '{pdir}'")
     if 'select' in context.op_config:
         s = context.op_config['select']        
         context.log.info(f"elt: executing dbt run with select: '{s}'")
-        context.resources.dbt.cli("test", project_dir=pdir, select=s)
+        context.resources.dbt.cli("test", select=s)
     else:
-        context.resources.dbt.cli("test", project_dir=pdir)
+        context.resources.dbt.cli("test")
 
 def _dbt_generate_docs(context):
-    pdir = context.op_config['project_dir']
+    pdir = context.resources.dbt.project_dir
     context.log.info(f"elt: executing dbt docs generate with project_dir: '{pdir}'")
     if 'select' in context.op_config:
         s = context.op_config['select']        
         context.log.info(f"elt: executing dbt run with select: '{s}'")
-        context.resources.dbt.cli("docs generate", project_dir=pdir, select=s)
+        context.resources.dbt.cli("docs generate", select=s)
     else:
-        context.resources.dbt.cli("docs generate", project_dir=pdir)
+        context.resources.dbt.cli("docs generate")
 
 def _dbt_source_freshness(context):
-    pdir = context.op_config['project_dir']
+    pdir = context.resources.dbt.project_dir
     context.log.info(f"elt: executing dbt source freshness with project_dir: '{pdir}'")
     if 'select' in context.op_config:
         s = context.op_config['select']        
         context.log.info(f"elt: executing dbt run with select: '{s}'")
-        context.resources.dbt.cli("source freshness", project_dir=pdir, select=s)
+        context.resources.dbt.cli("source freshness", select=s)
     else:
-        context.resources.dbt.cli("source freshness", project_dir=pdir)
+        context.resources.dbt.cli("source freshness")
 
-
+# Define dbt operations that use the dbt resource
 @op(required_resource_keys={'dbt'})
 def dbt_bronze_run_op(context, dependent_job=None):
     _dbt_run(context)
-    
+
 @op(required_resource_keys={'dbt'})
 def dbt_bronze_test_doc_sources_op(context, dependent_job=None):
     _dbt_test(context)
     _dbt_generate_docs(context)
     _dbt_source_freshness(context)
-
 
 @op(required_resource_keys={'dbt'})
 def dbt_silver_run_op(context, dependent_job=None):
@@ -65,7 +66,6 @@ def dbt_silver_test_doc_sources_op(context, dependent_job=None):
     _dbt_generate_docs(context)
     _dbt_source_freshness(context)
 
-
 @op(required_resource_keys={'dbt'})
 def dbt_gold_run_op(context, dependent_job=None):
     _dbt_run(context)
@@ -76,21 +76,20 @@ def dbt_gold_test_doc_sources_op(context, dependent_job=None):
     _dbt_generate_docs(context)
     _dbt_source_freshness(context)
 
-
-@job(resource_defs={'dbt': dbt_cli_resource})
+# Define jobs that use the dbt resource
+@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_bronze():
     dbt_bronze_test_doc_sources_op(dbt_bronze_run_op())
 
-
-@job(resource_defs={'dbt': dbt_cli_resource})
+@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_silver():    
     dbt_silver_test_doc_sources_op(dbt_silver_run_op())
 
-@job(resource_defs={'dbt': dbt_cli_resource})
+@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_gold():    
     dbt_gold_test_doc_sources_op(dbt_gold_run_op())
 
-@job(resource_defs={'dbt': dbt_cli_resource})
+@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir')})
 def dbt_all():    
     dbt_gold_test_doc_sources_op(
         dbt_gold_run_op(
--- projects/dagster/workspace.py
+++ projects/dagster/workspace.py
@@ -1,5 +1,5 @@
 from dagster import job, repository
-from dagster_dbt import dbt_cli_resource
+from dagster_dbt import DbtCliResource
 from dagster_pyspark import pyspark_resource
 
 from db import initialize_db, drop_tables_op, create_schemas_op, trino_resource
@@ -7,7 +7,7 @@ from download import download_yahoo_finance_files, download_yahoo_finance_files_
 from dbt import dbt_bronze, dbt_silver, dbt_gold, dbt_all, dbt_bronze_run_op, dbt_silver_run_op, dbt_gold_run_op, dbt_bronze_test_doc_sources_op, dbt_silver_test_doc_sources_op, dbt_gold_test_doc_sources_op
 from predict import predict_op
 
-@job(resource_defs={'dbt': dbt_cli_resource, 'trino': trino_resource, 'pyspark': pyspark_resource})
+@job(resource_defs={'dbt': DbtCliResource(project_dir='project_dir', profiles_dir='profiles_dir'), 'trino': trino_resource, 'pyspark': pyspark_resource})
 def e2e():    
     dbt_gold_test_doc_sources_op(
         dbt_gold_run_op(
@@ -29,11 +29,9 @@ def e2e():
         )
     )
 
-
 @job(resource_defs={'pyspark': pyspark_resource})
 def predict():
     predict_op()
-
 
 @repository
 def workspace():
